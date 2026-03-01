be780cf79ab86f118660fe9079e854d0f915e4c1
Daibisan <dibizeno@gmail.com>
2026-03-01 14:20:06 +0700

    refactor: changing category schema

--- prisma/migrations/20260301071158_init/migration.sql
+++ prisma/migrations/20260301071158_init/migration.sql
@@ -22,7 +22,7 @@ CREATE TABLE "Category" (
     "category_id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("category_id")
 );
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,16 +1,28 @@
 import { prisma } from "../lib/prisma.js";
 
 async function main() {
-    const user = await prisma.user.create({
-        data: {
-            username: "admin",
-            email: "admin@mail.com",
-            password_hash: "hashed123",
-            role: "ADMIN",
-        },
-    });
+    // const user = await prisma.user.create({
+    //     data: {
+    //         username: "admin",
+    //         email: "admin@mail.com",
+    //         password_hash: "hashed123",
+    //         role: "ADMIN",
+    //     },
+    // });
 
-    console.log(user);
+    // console.log(user);
+
+    // // test get user
+    // const user = await prisma.user.findMany();
+    // console.log(user);
+    
+    // test create category
+    const category = await prisma.category.create({data: { name: "drink"}});
+    console.log(category);
+
+
+    
+    prisma.$disconnect();
 }
 
 main();
