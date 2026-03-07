a4c660763f87d1c0cc1c4a20695fa8b815349a55
Daibisan <dibizeno@gmail.com>
2026-03-07 13:35:56 +0700

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
-export default {
-  test: {
-    globals: true, // Agar tidak perlu import describe, it, expect di tiap file
-    environment: 'node',
-    setupFiles: ['./tests/setup.ts'],
-  },
-};
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        reporters: ["tree"],
+        globals: true, // Agar tidak perlu import describe, it, expect di tiap file
+        environment: "node",
+        setupFiles: ["./tests/setup.ts"],
+        silent: true
+    },
+});
