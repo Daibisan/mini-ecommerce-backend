0bc60a1a51ed8cabe4c84fd8bb61c22a2bdf2018
Daibisan <dibizeno@gmail.com>
2026-03-07 09:17:00 +0700

    test: database cleaner setup

---
+++ tests/setup.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../src/lib/prisma.js";
+import { env } from "../src/config/env.js";
+import bcrypt from "bcrypt";
+import { ADMIN_ID } from "./test.config.js";
+
+beforeEach(async () => {
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+    await prisma.user.deleteMany();
+
+    // Add admin acc
+    const password_hash = await bcrypt.hash(env.ADMIN.pw, 11);
+    await prisma.user.upsert({
+        update: {},
+        create: {
+            user_id: ADMIN_ID,
+            username: env.ADMIN.email,
+            email: env.ADMIN.email,
+            password_hash,
+            role: "ADMIN",
+        },
+        where: { email: env.ADMIN.email },
+    });
+});
