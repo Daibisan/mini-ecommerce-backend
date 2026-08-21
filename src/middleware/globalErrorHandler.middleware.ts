2334db3977fa9ef6c1462eec64307f1e85c0f143
Daibisan <dibizeno@gmail.com>
2026-08-21 14:53:42 +0700

    feat: enhance global error handler to include handling for SyntaxError with status 400

--- src/middleware/globalErrorHandler.middleware.ts
+++ src/middleware/globalErrorHandler.middleware.ts
@@ -3,13 +3,23 @@ import AppError from "../utils/appError.util.js";
 import { env } from "../config/env.js";
 import { ApiResponse } from "../types/api.interface.js";
 
-export const globalErrorHandler: ErrorRequestHandler<{}, ApiResponse> = (err, req, res, next) => {
+export const globalErrorHandler: ErrorRequestHandler<{}, ApiResponse> = (
+    err,
+    req,
+    res,
+    next,
+) => {
     let statusCode = 500;
     let message = "Internal Server Error";
 
     if (err instanceof AppError) {
         statusCode = err.statusCode;
         message = err.message;
+    }
+
+    if (err instanceof SyntaxError && "status" in err && err.status === 400) {
+        statusCode = 400;
+        message = "Bad Request"
     }
 
     if (env.NODE_ENV !== "production" && message === "Internal Server Error") {
@@ -20,4 +30,4 @@ export const globalErrorHandler: ErrorRequestHandler<{}, ApiResponse> = (err, re
         success: false,
         error: message,
     });
-}
+};
